5d0dc9e6357c2f03db67435f93f79245a61b1f71
David Silal <dsilal@albany.edu>
2016-10-13 21:44:08 -0400

    Add reverse&RegularPrint

--- LinkList.java
+++ LinkList.java
@@ -8,6 +8,30 @@ public LinkList() {
 	tailNode = null;
 }
 
+//print the list in the forward direction
+public void print() {
+	ListNode element = headNode;
+	while (element != null)
+	{
+		System.out.print(element.getInfo());
+		if (element != tailNode)
+			System.out.print(" <=> ");
+		element = element.getTail();
+	}
+	System.out.print("\n");
+}
+
+//print the list in the backward direction
+public void reversePrint() {
+	ListNode element = tailNode;
+	while (element != null) {
+		System.out.print(element.getInfo());
+		if(element != headNode)
+			System.out.print(" <=> ");
+		element = element.getHead();
+	}
+	System.out.print("\n");
+}
 
 
 }
